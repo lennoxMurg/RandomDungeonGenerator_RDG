9057d23ac40202a133287f09951c23566c79fda5
Lennox <lennoxmurg@gmail.com>
2025-12-11 12:38:01 +0100

    Dungeon Generator code aus altem Programm eingefügt

--- RDG/Program.cs
+++ RDG/Program.cs
@@ -6,14 +6,14 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
-            const char wand = '#';
-            const char start = 'S';
-            const char ende = 'E';
+
+            const char WAND = '#';
+            const char START = 'S';
+            const char ENDE = 'E';
 
             const int breite_minimum = 10;
             const int breite_maximum = 50;
-
-            const int hoehe_minimum = 10;
+            const int hoehe_minimum = 10;               //NOCH GROßSCHREIBEN
             const int hoehe_maximum = 25;
 
             int breite = 0;
@@ -36,7 +36,6 @@ namespace Projekt
                         break;
                     }
 
-
                 }
                 catch (ArgumentException ex)
                 {
@@ -49,6 +48,8 @@ namespace Projekt
 
             }
             while (true);
+
+
 
         }
 
@@ -79,6 +80,70 @@ namespace Projekt
                 throw new ArgumentException("\nDie Höhe muss größer als 10 und kleiner als 25 sein.\n");
             }
             return hoehe;
+        }
+
+        static void InitialisiereDungeon(char[,] feld, char fuellZeichen)
+        {
+            int zeilen = feld.GetLength(0);
+            int spalten = feld.GetLength(1);
+
+            for (int i = 0; i < zeilen; i++)
+            {
+                for (int j = 0; j < spalten; j++)
+                {
+                    feld[i, j] = fuellZeichen;
+                }
+            }
+        }
+
+        static void PlatziereStartUndEnde(char[,] feld, Random zufall, char startZeichen, char endeZeichen)
+        {
+            int maxZeilen = feld.GetLength(0);
+            int maxSpalten = feld.GetLength(1);
+
+            // Zufallsbereich für nicht-randständige Positionen: [1, max-2]
+            // Zeile: zufall.Next(1, maxZeilen - 1)
+            // Spalte: zufall.Next(1, maxSpalten - 1)
+
+            // Startpunkt (S) platzieren
+            int startZeile = zufall.Next(1, maxZeilen - 1);
+            int startSpalte = zufall.Next(1, maxSpalten - 1);
+            feld[startZeile, startSpalte] = startZeichen;
+
+            // Endpunkt (E) platzieren, muss ein anderer Ort sein
+            int endeZeile, endeSpalte;
+            do
+            {
+                // Generiere neue zufällige Positionen
+                endeZeile = zufall.Next(1, maxZeilen - 1);
+                endeSpalte = zufall.Next(1, maxSpalten - 1);
+            }
+            // Wiederhole, falls Endpunkt gleich Startpunkt ist
+            while (endeZeile == startZeile && endeSpalte == startSpalte);
+
+            feld[endeZeile, endeSpalte] = endeZeichen;
+        }
+
+        static void GibDungeonAus(char[,] feld)
+        {
+            Console.WriteLine("--- ZUFALLS-DUNGEON ---");
+            Console.WriteLine();
+
+            int zeilen = feld.GetLength(0);
+            int spalten = feld.GetLength(1);
+
+            for (int i = 0; i < zeilen; i++)
+            {
+                for (int j = 0; j < spalten; j++)
+                {
+                    // Gib das Zeichen an der aktuellen Position aus
+                    Console.Write(feld[i, j]);
+                    // Optional: Fügen Sie ein Leerzeichen für eine bessere Lesbarkeit hinzu
+                    Console.Write(' ');
+                }
+                // Nach Abschluss einer Zeile: Neue Zeile einfügen
+                Console.WriteLine();
+            }
         }
 
     }
