7dfea424a79dbeb7996e054da61aa4d6c108981a
Lars <larsbecker300@gmail.com>
2025-12-15 11:48:05 +0100

    Farben für S und E

--- RDG/Program.cs
+++ RDG/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 
 namespace Projekt
 {
@@ -14,11 +13,9 @@ namespace Projekt
 
             const int BREITE_MINIMUM = 10;
             const int BREITE_MAXIMUM = 50;
-            const int HOEHE_MINIMUM = 10;               //NOCH GROßSCHREIBEN
+            const int HOEHE_MINIMUM = 10;
             const int HOEHE_MAXIMUM = 25;
 
-
-            
 
             int breite = 0;
             int hoehe = 0;
@@ -27,9 +24,8 @@ namespace Projekt
             {
                 try
                 {
-
+                    // Breiten- und Höhenabfrage
                     breite = breite_eingabe(breite, BREITE_MAXIMUM, BREITE_MINIMUM);
-
                     hoehe = hoehe_eingeben(hoehe, HOEHE_MAXIMUM, HOEHE_MINIMUM);
 
 
@@ -43,6 +39,13 @@ namespace Projekt
                 {
                     Console.WriteLine(ex.Message);
                 }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nFehler: Bitte geben Sie eine gültige ganze Zahl ein!\n");
+                    // Zurücksetzen der Werte bei Formatfehler, um die Schleife zu wiederholen
+                    breite = 0;
+                    hoehe = 0;
+                }
                 catch
                 {
                     Console.WriteLine("Es ist ein Unerwarteter Fehler aufgetreten!\n");
@@ -51,20 +54,20 @@ namespace Projekt
             }
             while (true);
 
-            // F.2.1 Datenstruktur: Zweidimensionales char-Array für die Dungeon-Karte
+            // Datenstruktur: Zweidimensionales char-Array für die Dungeon-Karte
             char[,] dungeonFeld = new char[breite, hoehe];
 
             // Für die zufällige Platzierung von Start und Ende
             Random zufall = new Random();
 
-            // --- F.2.2 Initialisierung: Array vollständig mit WALL füllen ---
+            // Initialisierung: Array vollständig mit WALL füllen
             InitialisiereDungeon(dungeonFeld, WAND);
 
-            // --- F.2.3 Start- und Endpunkt: Zufällige, nicht-randständige Platzierung ---
+            // Start- und Endpunkt: Zufällige, nicht-randständige Platzierung
             PlatziereStartUndEnde(dungeonFeld, zufall, START, ENDE);
 
-            // --- Ausgabe: Das gesamte Dungeon-Array ausgeben ---
-            GibDungeonAus(dungeonFeld);
+            // Ausgabe: Das gesamte Dungeon-Array ausgeben (mit Farbanpassung)
+            GibDungeonAus(dungeonFeld, START, ENDE); // START und ENDE werden für die Farbsteuerung übergeben
 
             Console.ReadKey();
 
@@ -72,6 +75,7 @@ namespace Projekt
 
         static int breite_eingabe(int breite, int breite_maximum, int breite_minimum)       // Eingabe Methode für breite
         {
+            // Wenn breite noch nicht gesetzt wurde (Wert 0), Eingabe fordern
             if (breite == 0)
             {
                 Console.WriteLine("Bitte die Breite Eingeben!");
@@ -80,21 +84,22 @@ namespace Projekt
                 if (breite < breite_minimum || breite > breite_maximum)
                 {
                     breite = 0;
-                    throw new ArgumentException("\nDie Breite muss größer als 10 und kleiner als 50 sein.\n");
+                    throw new ArgumentException($"\nDie Breite muss größer als {breite_minimum} und kleiner als {breite_maximum} sein.\n");
                 }
-
             }
             return breite;
         }
 
         static int hoehe_eingeben(int hoehe, int hoehe_maximum, int hoehe_minimum)      // Eingabe Methode für Höhe
         {
+            // Abfrage immer durchführen, da sie erst nach erfolgreicher Breiten-Eingabe erreicht wird.
             Console.WriteLine("Bitte die Höhe Eingeben!");
             hoehe = Convert.ToInt32(Console.ReadLine());
+
             if (hoehe < hoehe_minimum || hoehe > hoehe_maximum)
             {
                 hoehe = 0;
-                throw new ArgumentException("\nDie Höhe muss größer als 10 und kleiner als 25 sein.\n");
+                throw new ArgumentException($"\nDie Höhe muss größer als {hoehe_minimum} und kleiner als {hoehe_maximum} sein.\n");
             }
             return hoehe;
         }
@@ -115,53 +120,74 @@ namespace Projekt
 
         static void PlatziereStartUndEnde(char[,] feld, Random zufall, char startZeichen, char endeZeichen)
         {
-            int maxZeilen = feld.GetLength(0);
-            int maxSpalten = feld.GetLength(1);
+          
+            // Wir verwenden die Indizes wie sie deklariert wurden (i=Breite, j=Höhe)
+            int maxBreite = feld.GetLength(0); // I
+            int maxHoehe = feld.GetLength(1);  // J
 
-            // Zufallsbereich für nicht-randständige Positionen: [1, max-2]
-            // Zeile: zufall.Next(1, maxZeilen - 1)
-            // Spalte: zufall.Next(1, maxSpalten - 1)
-
+            // Zufallsbereich für nicht-randständige Positionen: [1, max-1] (exklusiv max)
             // Startpunkt (S) platzieren
-            int startZeile = zufall.Next(1, maxZeilen - 1);
-            int startSpalte = zufall.Next(1, maxSpalten - 1);
-            feld[startZeile, startSpalte] = startZeichen;
+            int startBreite = zufall.Next(1, maxBreite - 1);
+            int startHoehe = zufall.Next(1, maxHoehe - 1);
+            feld[startBreite, startHoehe] = startZeichen;
 
             // Endpunkt (E) platzieren, muss ein anderer Ort sein
-            int endeZeile, endeSpalte;
+            int endeBreite, endeHoehe;
             do
             {
                 // Generiere neue zufällige Positionen
-                endeZeile = zufall.Next(1, maxZeilen - 1);
-                endeSpalte = zufall.Next(1, maxSpalten - 1);
+                endeBreite = zufall.Next(1, maxBreite - 1);
+                endeHoehe = zufall.Next(1, maxHoehe - 1);
             }
             // Wiederhole, falls Endpunkt gleich Startpunkt ist
-            while (endeZeile == startZeile && endeSpalte == startSpalte);
+            while (endeBreite == startBreite && endeHoehe == startHoehe);
 
-            feld[endeZeile, endeSpalte] = endeZeichen;
+            feld[endeBreite, endeHoehe] = endeZeichen;
         }
 
-        static void GibDungeonAus(char[,] feld)
+        static void GibDungeonAus(char[,] feld, char startZeichen, char endeZeichen)
         {
-            Console.WriteLine("--- ZUFALLS-DUNGEON ---");
+            Console.WriteLine("\n--- ZUFALLS-DUNGEON ---");
             Console.WriteLine();
 
-            int zeilen = feld.GetLength(0);
-            int spalten = feld.GetLength(1);
+         
+            
 
-            for (int i = 0; i < zeilen; i++)
+            int maxBreite = feld.GetLength(0); // Erste Dimension
+            int maxHoehe = feld.GetLength(1);  // Zweite Dimension
+
+            // Äußere Schleife: Höhe (Zeilen)
+            for (int j = 0; j < maxHoehe; j++)
             {
-                for (int j = 0; j < spalten; j++)
+                // Innere Schleife: Breite (Spalten)
+                for (int i = 0; i < maxBreite; i++)
                 {
+                    char aktuellesZeichen = feld[i, j];
+
+                    // Prüfen, ob das Zeichen Start oder Ende ist
+                    if (aktuellesZeichen == startZeichen)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green; // Grün für Start
+                    }
+                    else if (aktuellesZeichen == endeZeichen)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red; // Rot für Ende 
+                    }
+                    else
+                    {
+                        Console.ResetColor(); // Standardfarbe für Wände
+                    }
+
                     // Gib das Zeichen an der aktuellen Position aus
-                    Console.Write(feld[i, j]);
-                    // Optional: Fügen Sie ein Leerzeichen für eine bessere Lesbarkeit hinzu
-                    Console.Write(' ');
+                    Console.Write(aktuellesZeichen);
+
+                    Console.Write(' '); // Abstand zwischen den Zeichen
                 }
-                // Nach Abschluss einer Zeile: Neue Zeile einfügen
+
+                // Am Ende jeder Zeile: Farbe zurücksetzen und neue Zeile einfügen
+                Console.ResetColor();
                 Console.WriteLine();
             }
         }
-
     }
 }
